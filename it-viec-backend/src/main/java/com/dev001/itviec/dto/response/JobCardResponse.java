9afcb816ada11ba239f6965259073f94f3f7348d
DEV00000001 <vuongdachaivang@gmail.com>
2026-05-07 23:29:30 +0700

    #1-feat: add JobCardResponse class for job listing representation

---
+++ it-viec-backend/src/main/java/com/dev001/itviec/dto/response/JobCardResponse.java
@@ -0,0 +1,25 @@
+package com.dev001.itviec.dto.response;
+
+import com.dev001.itviec.enums.JobType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class JobCardResponse {
+    Long id;
+    String title;
+    String slug;
+    CityResponse city;
+    String salary;
+    JobType jobType;
+    LocalDateTime postedAt;
+    Set<SkillResponse> skills;
+    CompanySummaryResponse company;
+}
