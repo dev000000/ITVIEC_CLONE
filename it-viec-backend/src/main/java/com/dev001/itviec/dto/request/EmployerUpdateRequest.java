d79c97ba680d6315721270ec2d2b690bcca17966
DEV00000001 <vuongdachaivang@gmail.com>
2026-05-14 23:23:58 +0700

    #1-refactor: clean up imports and organize code structure across multiple files

--- it-viec-backend/src/main/java/com/dev001/itviec/dto/request/EmployerUpdateRequest.java
+++ it-viec-backend/src/main/java/com/dev001/itviec/dto/request/EmployerUpdateRequest.java
@@ -21,5 +21,4 @@ public class EmployerUpdateRequest {
     @NotBlank(message = "PHONE_NUMBER_REQUIRED")
     @Pattern(regexp = "^(0(3[2-9]|5[6-9]|7[0|6-9]|8[0-9]|9[0-9]))[0-9]{7}$", message = "PHONE_NUMBER_INVALID")
     String phoneNumber;
-
 }
